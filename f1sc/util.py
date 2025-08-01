0d569aa6f3910acd3dbbd804b44d5c506b886333
Alex George <107772948+algx23@users.noreply.github.com>
2025-08-01 19:54:56 +0100

    refactor: add create_slider function
    - add create_slider function to utils to create a pyqt advanced slider based on input parameters(min, max, is_float)

--- f1sc/util.py
+++ f1sc/util.py
@@ -3,6 +3,7 @@ this file contains certain utility functions which make code a little bit cleane
 """
 
 from PyQt6.QtWidgets import QLabel
+from pyqt_advanced_slider import Slider
 
 
 def create_label(text: str, data_category: str = None) -> QLabel:
@@ -11,3 +12,12 @@ def create_label(text: str, data_category: str = None) -> QLabel:
     if data_category:
         label.setObjectName(data_category)
     return label
+
+
+def create_slider(min: int | float, max: int | float, is_float: bool) -> Slider:
+    slider = Slider()
+    if is_float:
+        slider.setFloat(True)
+    slider.setMinimum(min)
+    slider.setMaximum(max)
+    return slider

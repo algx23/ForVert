144d03c5f6b71cad4528f50a4840463f80b49919
Alex George <107772948+algx23@users.noreply.github.com>
2025-08-10 19:24:39 +0100

    fix: update tests to use SetupUpdater class
     - change tests to update values using the F1SetupUpdater class rather than just importing methods

--- tests/test_conversions.py
+++ tests/test_conversions.py
@@ -1,76 +1,97 @@
 import pytest
 
-from f1sc.main import (
-    update_aerodynamics,
-    update_transmission,
-    update_suspension_geometry,
-    update_brakes,
-    update_suspension,
-    update_tires,
-)
+from f1sc.SetupUpdater import F1SetupUpdater
 
 
 def test_aero_update():
-    input_aero_vals: dict = {"Front Wing Aero": 3, "Rear Wing Aero": 5}
-    updated_aero_vals: tuple = update_aerodynamics(input_aero_vals)
-    assert updated_aero_vals[0] == 5 and updated_aero_vals[1] == 7
+    input_aero_vals: dict = {
+        "Aerodynamics": {"Front Wing Aero": 3, "Rear Wing Aero": 5}
+    }
+    updater = F1SetupUpdater(input_aero_vals)
+    updated_aero_vals = updater.update_aerodynamics()
+    assert updated_aero_vals["Front Wing"] == 5
+    assert updated_aero_vals["Rear Wing"] == 7
 
 
 def test_transmission_update():
     input_transmission_vals: dict = {
-        "Differential Adjustment On Throttle (%)": 75,
-        "Differential Adjustment Off throttle (%)": 69,
+        "Transmission": {
+            "Differential Adjustment On Throttle (%)": 75,
+            "Differential Adjustment Off throttle (%)": 69,
+        }
     }
-    update_diff_on, update_diff_off = update_transmission(input_transmission_vals)
+    updater = F1SetupUpdater(input_transmission_vals)
 
+    updated_transmission_vals = updater.update_transmission()
+    update_diff_on, update_diff_off = (
+        updated_transmission_vals["Diff On Throttle"],
+        updated_transmission_vals["Diff Off Throttle"],
+    )
     assert update_diff_on == 50
     assert update_diff_off == 69
 
 
 def test_transmission_over_70():
     input_transmission_vals: dict = {
-        "Differential Adjustment On Throttle (%)": 75,
-        "Differential Adjustment Off throttle (%)": 76,  # this is the thing to test
+        "Transmission": {
+            "Differential Adjustment On Throttle (%)": 75,
+            "Differential Adjustment Off throttle (%)": 76,  # this is the thing to test
+        }
     }
-    update_diff_off = update_transmission(input_transmission_vals)[1]
+    updater = F1SetupUpdater(input_transmission_vals)
+    update_diff_off = updater.update_transmission()["Diff Off Throttle"]
     assert update_diff_off == 65
 
 
 def test_suspension_geometry():
     input_sus_geo = {
-        "Front Camber": 1,
-        "Rear Camber": -2.5,
-        "Front Toe": 1,
-        "Rear Toe": 1,
+        "Suspension Geometry": {
+            "Front Camber": 1,
+            "Rear Camber": -2.5,
+            "Front Toe": 1,
+            "Rear Toe": 1,
+        }
     }
-    updated_sus_geo = update_suspension_geometry(input_sus_geo)
-    assert updated_sus_geo[1] == -2.6
+    updater = F1SetupUpdater(input_sus_geo)
+    updated_sus_geo = updater.update_suspension_geometry()
+    assert updated_sus_geo["Rear Camber"] == -2.6
 
 
 def test_sus_geo_limit():
     input_sus_geo = {
-        "Front Camber": 1,
-        "Rear Camber": -3.5,
-        "Front Toe": 1,
-        "Rear Toe": 1,
+        "Suspension Geometry": {
+            "Front Camber": 1,
+            "Rear Camber": -3.5,
+            "Front Toe": 1,
+            "Rear Toe": 1,
+        }
     }
-    updated_sus_geo = update_suspension_geometry(input_sus_geo)
-    assert updated_sus_geo[1] == -3.5
+    updater = F1SetupUpdater(input_sus_geo)
+    updated_sus_geo = updater.update_suspension_geometry()
+    assert updated_sus_geo["Rear Camber"] == -3.5
 
 
 def test_suspension():
     input_sus_vals = {
-        "Front Suspension (soft - firm)": 3,
-        "Rear Suspension (soft - firm)": 3,
-        "Front Anti Roll Bar": 1,
-        "Rear Anti Roll Bar": 4,
-        "Front Ride Height": 1,
-        "Rear Ride Height": 1,
+        "Suspension": {
+            "Front Suspension (soft - firm)": 3,
+            "Rear Suspension (soft - firm)": 3,
+            "Front Anti Roll Bar": 1,
+            "Rear Anti Roll Bar": 4,
+            "Front Ride Height": 1,
+            "Rear Ride Height": 1,
+        }
     }
+    updater = F1SetupUpdater(input_sus_vals)
 
-    front_sus, rear_sus, front_arb, rear_arb, front_rh, rear_rh = update_suspension(
-        input_sus_vals
+    updated_sus = updater.update_suspension()
+    front_sus, rear_sus = (
+        updated_sus["Front Suspension"],
+        updated_sus["Rear Suspension"],
     )
+    rear_arb = updated_sus["Rear Anti Roll Bar"]
+    front_rh = updated_sus["Front Ride Height"]
+    rear_rh = updated_sus["Rear Ride Height"]
     assert front_sus == 2
     assert rear_sus == 2
     assert rear_arb == 4
@@ -80,17 +101,21 @@ def test_suspension():
 
 def test_suspension_conditional_update():
     input_sus_vals = {
-        "Front Suspension (soft - firm)": 7,
-        "Rear Suspension (soft - firm)": 8,
-        "Front Anti Roll Bar": 1,
-        "Rear Anti Roll Bar": 9,
-        "Front Ride Height": 1,
-        "Rear Ride Height": 1,
+        "Suspension": {
+            "Front Suspension (soft - firm)": 7,
+            "Rear Suspension (soft - firm)": 8,
+            "Front Anti Roll Bar": 1,
+            "Rear Anti Roll Bar": 9,
+            "Front Ride Height": 1,
+            "Rear Ride Height": 1,
+        }
     }
+    updater = F1SetupUpdater(input_sus_vals)
 
-    front_sus, rear_sus, front_arb, rear_arb, front_rh, rear_rh = update_suspension(
-        input_sus_vals
-    )
+    updated_sus = updater.update_suspension()
+    front_sus = updated_sus["Front Suspension"]
+    rear_sus = updated_sus["Rear Suspension"]
+    rear_arb = updated_sus["Rear Anti Roll Bar"]
 
     assert front_sus == 5
     assert rear_sus == 6
@@ -98,14 +123,20 @@ def test_suspension_conditional_update():
 
 
 def test_brakes():
-    input_brake_vals = {"Brake Pressure %": 55, "Brake Bias (Front ---  Rear) %": 98}
-    updated_pressure, updated_bias = update_brakes(input_brake_vals)
-    assert updated_pressure == 89
-    assert updated_bias == 52
+    input_brake_vals = {
+        "Brakes": {"Brake Pressure %": 55, "Brake Bias (Front ---  Rear) %": 98}
+    }
+    updater = F1SetupUpdater(input_brake_vals)
+    updated_brakes = updater.update_brakes()
+    assert updated_brakes["Brake Pressure"] == 89
+    assert updated_brakes["Brake Bias"] == 52
 
 
 def test_tires():
-    input_tire_vals = {"Front Tire Pressure": 21.8, "Rear Tire Pressure": 21.1}
-    front, rear = update_tires(input_tire_vals)
-    assert front == 21.4
-    assert rear == 20.3
+    input_tire_vals = {
+        "Tires": {"Front Tire Pressure": 21.8, "Rear Tire Pressure": 21.1}
+    }
+    updater = F1SetupUpdater(input_tire_vals)
+    updated_tires = updater.update_tires()
+    assert updated_tires["Front Tire Pressure"] == 21.4
+    assert updated_tires["Rear Tire Pressure"] == 20.3
